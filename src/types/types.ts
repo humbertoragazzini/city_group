50c8f18cffbc4d6fb92848a5fb0c96ae8b7d7c44
beto <beto_vicio@hotmail.com>
2023-09-30 13:29:19 +0100

    added new dinamic content

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,23 @@
+import { ReactHTMLElement } from "react";
+
+export interface Titles {
+  text:String;
+  content:String;
+}
+
+export interface Services {
+  text:String;
+  content:String;
+  icon: any;
+}
+
 
 export interface Item {
   isFirst: Boolean;
-  titleFirst: string;
+  titleFirst: string[];
+  WSProvide: String;
+  EOservices: Titles[];
+  TOSProvided: Services[]
   titleSecond: string;
   backgroundURL:string;
   left: Boolean;
