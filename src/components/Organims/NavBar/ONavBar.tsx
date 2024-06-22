afa28b56077375b84da772555ee0deba40107d80
humberto ragazzini <beto_vicio@hotmail.com>
2024-06-22 14:49:31 +0200

    added menu open and close btn

--- src/components/Organims/NavBar/ONavBar.tsx
+++ src/components/Organims/NavBar/ONavBar.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CNavBarLink from "../../Cells/NavBar/CNavBarLink";
 import CNavMobLink from "@/components/Cells/NavBar/CNavMobLink";
 
 export default function ONavBar() {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   useEffect(() => {
     console.log("Current route:", pathname);
@@ -54,49 +55,58 @@ export default function ONavBar() {
         </div>
       </nav>
       <nav className="fixed w-full flex flex-row lg:hidden justify-end max-w-[1280px] mx-auto">
-        <button className="bg-red-200 w-[50px] h-[50px] m-2 absolute z-50"></button>
-        <div className="backdrop-blur-sm w-full">
-          <div className="m-2">
-            <CNavMobLink
-              className={
-                pathname === "/" ? "bg-the-red text-white shadow-lg" : ""
-              }
-              linkText={"Home"}
-              href={"/"}
-            ></CNavMobLink>
+        <button
+          className="bg-red-200 w-[50px] h-[50px] m-2 absolute z-50"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        ></button>
+        {isOpen && (
+          <div className="backdrop-blur-sm w-full">
+            <div className="m-2">
+              <CNavMobLink
+                className={
+                  pathname === "/" ? "bg-the-red text-white shadow-lg" : ""
+                }
+                linkText={"Home"}
+                href={"/"}
+              ></CNavMobLink>
+            </div>
+            <div className="m-2">
+              <CNavMobLink
+                className={
+                  pathname === "/OurServices"
+                    ? "bg-the-red text-white shadow-lg"
+                    : ""
+                }
+                linkText={"Our services"}
+                href={"/OurServices"}
+              ></CNavMobLink>
+            </div>
+            <div className="m-2">
+              <CNavMobLink
+                className={
+                  pathname === "/AboutUs"
+                    ? "bg-the-red text-white shadow-lg"
+                    : ""
+                }
+                linkText={"About us"}
+                href={"/AboutUs"}
+              ></CNavMobLink>
+            </div>
+            <div className="m-2">
+              <CNavMobLink
+                className={
+                  pathname === "/ContactUs"
+                    ? "bg-the-red text-white shadow-lg"
+                    : ""
+                }
+                linkText={"Contact us"}
+                href={"/ContactUs"}
+              ></CNavMobLink>
+            </div>
           </div>
-          <div className="m-2">
-            <CNavMobLink
-              className={
-                pathname === "/OurServices"
-                  ? "bg-the-red text-white shadow-lg"
-                  : ""
-              }
-              linkText={"Our services"}
-              href={"/OurServices"}
-            ></CNavMobLink>
-          </div>
-          <div className="m-2">
-            <CNavMobLink
-              className={
-                pathname === "/AboutUs" ? "bg-the-red text-white shadow-lg" : ""
-              }
-              linkText={"About us"}
-              href={"/AboutUs"}
-            ></CNavMobLink>
-          </div>
-          <div className="m-2">
-            <CNavMobLink
-              className={
-                pathname === "/ContactUs"
-                  ? "bg-the-red text-white shadow-lg"
-                  : ""
-              }
-              linkText={"Contact us"}
-              href={"/ContactUs"}
-            ></CNavMobLink>
-          </div>
-        </div>
+        )}
       </nav>
     </>
   );
