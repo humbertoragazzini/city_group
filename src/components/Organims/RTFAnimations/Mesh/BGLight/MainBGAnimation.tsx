209dcd872c5f5c277df53a1cb3c991701df10c63
humberto ragazzini <beto_vicio@hotmail.com>
2024-07-03 20:26:08 +0200

    cleaning the code

---
+++ src/components/Organims/RTFAnimations/Mesh/BGLight/MainBGAnimation.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { Environment } from "@react-three/drei";
+
+export default function MainBGAnimation() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {/* Environment */}
+      <Environment
+        background
+        blur={2}
+        backgroundBlurriness={0}
+        backgroundIntensity={1}
+        files={"./RTFA/enviromentMap/blender_2kv3.hdr"}
+      ></Environment>
+
+      {/* Ligth */}
+      <directionalLight
+        position={[1, 1, 1]}
+        intensity={5}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-camera-far={50}
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+        shadow-camera-top={10}
+        shadow-camera-bottom={-10}
+        shadow-bias={-0.0005}
+      ></directionalLight>
+    </>
+  );
+}
