6cd7ddc3215fac2cc4afe3a9b69e12a85cbd18e1
humbertoragazzini <beto_vicio@hotmail.com>
2025-01-28 22:21:47 +0000

    added bg-gradient to the main menu

--- src/components/Organims/NavBar/ONavBar.tsx
+++ src/components/Organims/NavBar/ONavBar.tsx
@@ -8,7 +8,7 @@ export default function ONavBar() {
   return (
     <>
       {context.state.isMenuEnable && (
-        <div className="fixed bg-transparent h-[90px] top-0 overflow-visible w-screen z-50">
+        <div className="fixed bg-gradient-to-b from-black to-transparent h-[90px] top-0 overflow-visible w-screen z-50">
           <button
             className={`group fixed left-4 top-4 z-0 h-12 w-12 rounded-tr-full rounded-tl-full rounded-br-full rounded-bl-full [&>svg]:stroke-black hover:bg-the-lightGrey hover:[&>svg]:stroke-white transition-all animate-rounded-change-disable bg-black "
         }`}
