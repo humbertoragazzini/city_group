9780840339b381c1b3a92db93b9eede0784b21c5
humbertoragazzini <beto_vicio@hotmail.com>
2025-03-01 19:57:29 +0000

    added containers

--- src/app/ComponentDisplay/page.tsx
+++ src/app/ComponentDisplay/page.tsx
@@ -257,9 +257,28 @@ export default function ComponentDisplay() {
       <hr className="mt-10 mb-6 border-4 border-black"></hr>
       <h1 className="mb-6 text-3xl font-bold">CONTAINERS</h1>
       <div className="flex items-center justify-center w-full">
-        <div className="relative w-full h-[750px] px-5">
+        <div className="relative w-full px-5 h-fit">
           <div className="relative w-full h-full">
-            <div className="h-full bg-red 2-full skew-x-12"></div>
+            <div className="absolute z-0 w-full h-full bg-red skew-x-12"></div>
+            <div className="relative z-10 w-full h-full py-10 px-20">
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source. Lorem Ipsum comes
+              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
+              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
+              BC. This book is a treatise on the theory of ethics, very popular
+              during the Renaissance. The first line of Lorem Ipsum, "Lorem
+              ipsum dolor sit amet..", comes from a line in section 1.10.32. The
+              standard chunk of Lorem Ipsum used since the 1500s is reproduced
+              below for those interested. Sections 1.10.32 and 1.10.33 from "de
+              Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+              exact original form, accompanied by English versions from the 1914
+              translation by H. Rackham.
+            </div>
           </div>
         </div>
       </div>
