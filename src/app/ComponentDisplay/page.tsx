6df6de789fb95ef5eb2101e22f9163db20b22381
humbertoragazzini <beto_vicio@hotmail.com>
2025-03-01 20:03:39 +0000

    added containers with text

--- src/app/ComponentDisplay/page.tsx
+++ src/app/ComponentDisplay/page.tsx
@@ -134,7 +134,6 @@ export default function ComponentDisplay() {
     <div className="p-6 bg-white">
       <hr className="mt-10 mb-6 border-4 border-black"></hr>
       <h1 className="mb-6 text-3xl font-bold">BUTTONS</h1>
-
       <h2 className="mt-8 mb-4 text-xl font-bold">With text</h2>
       {/* Full-Width Buttons */}
       <div className="flex flex-wrap items-center gap-4">
@@ -157,9 +156,7 @@ export default function ComponentDisplay() {
           </button>
         ))}
       </div>
-
       <h2 className="mt-8 mb-4 text-xl font-bold">Icon only</h2>
-
       {/* Icon-Only Buttons */}
       <div className="flex flex-wrap items-center gap-4">
         {buttonThemes.map(({ bg, text, hover }, index) => (
@@ -171,7 +168,6 @@ export default function ComponentDisplay() {
           </button>
         ))}
       </div>
-
       <hr className="mt-10 mb-6 border-4 border-black"></hr>
       <h1 className="mb-6 text-3xl font-bold">HEADINGS</h1>
       <div className="">
@@ -255,33 +251,53 @@ export default function ComponentDisplay() {
         })}
       </div>
       <hr className="mt-10 mb-6 border-4 border-black"></hr>
-      <h1 className="mb-6 text-3xl font-bold">CONTAINERS</h1>
-      <div className="flex items-center justify-center w-full">
-        <div className="relative w-full px-5 h-fit">
-          <div className="relative w-full h-full">
-            <div className="absolute z-0 w-full h-full bg-red skew-x-12"></div>
-            <div className="relative z-10 w-full h-full py-10 px-20">
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source. Lorem Ipsum comes
-              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
-              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
-              BC. This book is a treatise on the theory of ethics, very popular
-              during the Renaissance. The first line of Lorem Ipsum, "Lorem
-              ipsum dolor sit amet..", comes from a line in section 1.10.32. The
-              standard chunk of Lorem Ipsum used since the 1500s is reproduced
-              below for those interested. Sections 1.10.32 and 1.10.33 from "de
-              Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-              exact original form, accompanied by English versions from the 1914
-              translation by H. Rackham.
-            </div>
+      <h1 className="mb-6 text-3xl font-bold">CONTAINERS</h1>{" "}
+      {paragraphSizes.map(({ type }, indexA) => {
+        return (
+          <div
+            className="flex flex-col items-start justify-start my-4"
+            key="indexA"
+          >
+            <h1 className="text-xl">{type}</h1>{" "}
+            {paragraphThemes.map(({ bg, text }, index) => (
+              <div className="flex items-center justify-center w-full p-10">
+                <div className="relative w-full h-full">
+                  <div
+                    className={`absolute z-0 w-full h-full ${bg} skew-x-12`}
+                  ></div>
+                  <div className="relative z-10 w-full h-full px-20 py-10">
+                    <Paragraph
+                      type={type}
+                      key={index}
+                      className={`${text} z-0 transition-all duration-500 ${bg}`}
+                      text={[
+                        {
+                          language: "EN",
+                          content: "Retail (Ingles)",
+                        },
+                        {
+                          language: "ES",
+                          content:
+                            "Titulo secundario de la pagina standfirst, al por Menor (Espaniol)",
+                        },
+                        {
+                          language: "CH",
+                          content: "进口、建筑和零售的综合解决方案 (Chino)",
+                        },
+                        {
+                          language: "IT",
+                          content:
+                            "Titulo secundario de la pagina standfirst, al Dettaglio (Italiano)",
+                        },
+                      ]}
+                    ></Paragraph>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
