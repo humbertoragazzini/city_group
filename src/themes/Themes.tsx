bfe4427cc1a2537bcd82060d5fa58657a2094b2d
humbertoragazzini <beto_vicio@hotmail.com>
2025-03-02 13:47:10 +0000

    now th theme affect the bg and the text in angled container

---
+++ src/themes/Themes.tsx
@@ -0,0 +1,11 @@
+export default {
+	deepBlack: { bg: "bg-deepBlack", text: "text-yellowBright" },
+	plum: { bg: "bg-plum", text: "text-white" },
+	burgundy: { bg: "bg-burgundy", text: "text-white" },
+	white: { bg: "bg-white", text: "text-black" },
+	lime: { bg: "bg-lime", text: "text-black" },
+	yellowBright: { bg: "bg-yellowBright", text: "text-black" },
+	graphite: { bg: "bg-graphite", text: "text-white" },
+	black: { bg: "bg-black", text: "text-white" },
+	rose: { bg: "bg-rose", text: "text-white" },
+};
