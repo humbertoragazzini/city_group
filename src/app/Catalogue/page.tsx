e49b7d37bb55d14c32790397daf3e8bc95109c9a
humbertoragazzini <beto_vicio@hotmail.com>
2025-03-16 23:40:26 +0000

    added photo and bg

--- src/app/Catalogue/page.tsx
+++ src/app/Catalogue/page.tsx
@@ -26,12 +26,18 @@ export default function Catalogue() {
     >
       {/* Section with us and the description */}
       <div className="w-full min-h-screen py-24 bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,1)] flex flex-col justify-center items-center overflow-hidden relative">
-        <div className="absolute z-10 h-full min-w-full min-h-screen"></div>
-        <div className="grid grid-cols-2 w-full xl:w-[75%] xl:max-w-[1920px] p-1 md:p-2 lg:p-4">
-          <div className="col-span-2 backdrop-blur-md p-5 bg-[rgba(255,255,255,0.2)] h-[500px]">
-            <h1>Product name</h1>
-            <h2>Product model</h2>
-            <p>Description of the product</p>
+        <div className="grid w-full grid-cols-1 p-4 lg:p-8">
+          <div className="col-span-1">
+            <div className="grid grid-cols-2 w-full backdrop-blur-md p-5 bg-[rgba(255,255,255,0.2)]">
+              <div className="col-span-1">
+                <h1>Product name</h1>
+                <h2>Product model</h2>
+                <p>Description of the product</p>
+              </div>
+              <div className="col-span-1">
+                <img src="https://placehold.co/300x300"></img>
+              </div>
+            </div>
           </div>
         </div>
       </div>
