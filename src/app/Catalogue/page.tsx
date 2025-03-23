67f04f3e99321728d0b931dcb38bb157fe5821cf
humbertoragazzini <beto_vicio@hotmail.com>
2025-03-23 21:05:58 +0000

    fixing carousel

--- src/app/Catalogue/page.tsx
+++ src/app/Catalogue/page.tsx
@@ -146,24 +146,24 @@ export default function Catalogue() {
     >
       <div className="w-full flex justify-center items-bcenter p-8"><h1 className="text-3xl font-bold">Filter</h1></div>
       <div className="bg-[rgba(255,255,255,0.2)] rounded-xl m-4 lg:mx-8 p-4 grid grid-cols-4">
-        <div className="col-span-4 lg:col-span-1 px-5 py-3 flex lg:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
           <label className="mr-2">By ID:</label>
           <input onKeyUp={(e) => { setIDtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by ID">
           </input>
         </div>
-        <div className="col-span-4 lg:col-span-1 px-5 py-3 flex lg:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
           <label className="mr-2">By name:</label>
           <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by word">
           </input>
         </div>
-        <div className="col-span-4 lg:col-span-1 px-5 py-3 flex lg:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
           <label className="mr-2">By category:</label>
           {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
           <DropdownMenu category={category}
             setCategory={setCategory} selected={selected}
             setSelected={setSelected} types={products} products={products}></DropdownMenu>
         </div>
-        <div className="col-span-4 lg:col-span-1 px-5 py-3 flex lg:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
           <label className="mr-2">By subcategory:</label>
           {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
           <DropdownMenu category={category}
@@ -264,11 +264,11 @@ function Item({ product, index }: any) {
   const itemRef = useRef();
 
   useLayoutEffect(() => {
-    gsap.fromTo(itemRef.current, { opacity: 0 }, { opacity: 1, delay: index / 10 })
+    // gsap.fromTo(itemRef.current, { opacity: 0 }, { opacity: 1, delay: index / 10 })
   }, [])
 
   return (
-    <div ref={itemRef} style={{ opacity: 0, }} className={`relative col-span-1 rounded-xl ${enable ? "border-2 border-[rgba(255,255,255,0.35)] mt-3 mb-3 p-2" : "border-3 border-[rgba(255,255,255,0)] mt-2 bg-transparent"} transition-all duration-500`}>
+    <div ref={itemRef} style={{ opacity: 1, }} className={`relative col-span-1 rounded-xl ${enable ? "border-2 border-[rgba(255,255,255,0.35)] mt-3 mb-3 p-2" : "border-3 border-[rgba(255,255,255,0)] mt-2 bg-transparent"} transition-all duration-500`}>
       <button onClick={e => setEnable(!enable)} className="relative z-10 grid grid-cols-12 full backdrop-blur-md p-2 bg-[rgba(255,255,255,0.2)] rounded-xl cursor-pointer w-full hover:bg-[rgba(255,255,255,0.30)] transition-all duration-300">
         <div className="col-span-3 md:col-span-2 p-2 border-r-2 border-[rgba(255,255,255,0.5)]">
           <div className="flex justify-center items-center">
@@ -294,7 +294,7 @@ function Item({ product, index }: any) {
       <motion.div animate={{ height: !enable ? "0px" : "auto", marginTop: !enable ? "0px" : "8px" }} className="z-0 h-[0px] grid grid-cols-1 w-full overflow-hidden">
         <div className="col-span-1">
           <motion.div className="z-0 top-0 left-0 grid grid-cols-10 w-full backdrop-blur-md bg-[rgba(255,255,255,0.2)] rounded-xl">
-            <div className="col-span-10 lg:col-span-7 p-3 lg:p-12">
+            <div className="col-span-10 lg:col-span-5 p-3 lg:p-12">
               <div className="w-full border-b-2 border-[rgba(255,255,255,0.5)] mb-4">
                 <h1 className="text-2xl pb-2">Camara de seguridad 1080P/4k</h1>
               </div>
@@ -318,8 +318,10 @@ function Item({ product, index }: any) {
                 <div className="text-lg">Lorem Ipsum is simply...</div>
               </div>
             </div>
-            <div className="col-span-10 lg:col-span-3 lg:p-8">
-              <CardCarousel></CardCarousel>
+            <div className="col-span-10 lg:col-span-5 lg:p-8">
+              <div className="w-full">
+                <CardCarousel></CardCarousel>
+              </div>
             </div>
           </motion.div>
         </div>
