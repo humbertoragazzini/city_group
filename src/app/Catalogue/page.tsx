bdc5237f6a1ac190ceebeb07673d849b1c09f357
humbertoragazzini <beto_vicio@hotmail.com>
2025-03-26 20:02:05 +0000

    adding style to the search bar

--- src/app/Catalogue/page.tsx
+++ src/app/Catalogue/page.tsx
@@ -44,62 +44,64 @@ export default function Catalogue() {
 
   return (
     <div
-      className={`relative z-10 transition-all duration-1000 m-auto pt-[0px] text-white`}
+      className={`relative z-10 transition-all duration-1000 m-auto pt-[0px] bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,1)] min-h-screen w-full text-white`}
     >
-      <div className="w-full flex justify-center items-bcenter p-8"><h1 className="text-3xl font-bold">Filter</h1></div>
-      <div className="bg-[rgba(255,255,255,0.2)] rounded-xl m-4 lg:mx-8 p-4 grid grid-cols-4">
-        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
-          <label className="mr-2">By ID:</label>
-          <input onKeyUp={(e) => { setIDtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by ID">
-          </input>
+      <div className="w-full max-w-[1450px] mx-auto">
+        <div className="rounded-xl m-4 lg:mx-8 p-2 grid grid-cols-4">
+          <div className="col-span-4 md:col-span-2 xl:col-span-1 px-3 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+            <label className="mr-2">By ID:</label>
+            <input onKeyUp={(e) => { setIDtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by ID">
+            </input>
+          </div>
+          <div className="col-span-4 md:col-span-2 xl:col-span-1 px-3 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+            <label className="mr-2">By name:</label>
+            <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by word">
+            </input>
+          </div>
+          <div className="col-span-4 md:col-span-2 xl:col-span-1 px-3 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+            <label className="mr-2">By category:</label>
+            {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
+            <DropdownMenu category={category}
+              setCategory={setCategory} selected={selected}
+              setSelected={setSelected} types={products} products={products} type="category"></DropdownMenu>
+          </div>
+          <div className="col-span-4 md:col-span-2 xl:col-span-1 px-3 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
+            <label className="mr-2">By subcategory:</label>
+            {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
+            <DropdownMenu category={subCategory}
+              setCategory={setSubCategory} selected={subSelected} filtered={filtered}
+              setSelected={setSubSelected} types={products} products={products}></DropdownMenu>
+          </div>
         </div>
-        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
-          <label className="mr-2">By name:</label>
-          <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by word">
-          </input>
-        </div>
-        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
-          <label className="mr-2">By category:</label>
-          {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
-          <DropdownMenu category={category}
-            setCategory={setCategory} selected={selected}
-            setSelected={setSelected} types={products} products={products} type="category"></DropdownMenu>
-        </div>
-        <div className="col-span-4 md:col-span-2 xl:col-span-1 px-5 py-3 flex justify-start xl:justify-center items-center font-semibold mx-2 text-white rounded-lg">
-          <label className="mr-2">By subcategory:</label>
-          {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
-          <DropdownMenu category={subCategory}
-            setCategory={setSubCategory} selected={subSelected} filtered={filtered}
-            setSelected={setSubSelected} types={products} products={products}></DropdownMenu>
-        </div>
-      </div>
-      {/* Section with us and the description */}
-      <div ref={resultContainerRef} className="w-full min-h-screen bg-gradient-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,1)] flex flex-col justify-start items-center overflow-hidden relative">
-        <div className="grid w-full grid-cols-1 p-4 lg:p-8">
-          <button className="relative z-10 grid grid-cols-12 full backdrop-blur-md p-2 rounded-xl cursor-pointer w-full transition-all duration-300">
-            <div className="col-span-3 md:col-span-2 p-2 border-r-2 border-[rgba(255,255,255,0.5)]">
-              <div className="flex justify-center items-center">
-                <p className="font-bold w-full text-center">ID</p>
+        {/* Section with us and the description */}
+        <div ref={resultContainerRef} className="w-full flex flex-col justify-start items-center overflow-hidden relative">
+          <div className="grid w-full grid-cols-1 p-4 lg:p-8">
+            <button className="relative z-10 grid grid-cols-12 full backdrop-blur-md p-2 rounded-xl cursor-pointer w-full transition-all duration-300">
+              <div className="col-span-3 md:col-span-2 p-2 border-r-2 border-[rgba(255,255,255,0.5)]">
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-center">ID</p>
+                </div>
               </div>
-            </div>
-            <div className="col-span-9 p-2 md:col-span-6 md:border-r-2 border-[rgba(255,255,255,0.5)]">
-              <div className="flex justify-center items-center">
-                <p className="font-bold w-full text-left">Name</p>
+              <div className="col-span-9 p-2 md:col-span-6 md:border-r-2 border-[rgba(255,255,255,0.5)]">
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-left">Name</p>
+                </div>
               </div>
-            </div>
-            <div className="col-span-4 hidden p-2 md:block md:col-span-3 md:border-r-2 border-[rgba(255,255,255,0.5)]">
-              <div className="flex justify-center items-center">
-                <p className="font-bold w-full text-center">Category</p>
+              <div className="col-span-4 hidden p-2 md:block md:col-span-3 md:border-r-2 border-[rgba(255,255,255,0.5)]">
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-center">Category</p>
+                </div>
               </div>
-            </div>
-            <div className="col-span-1 hidden lg:block md:grid p-2">
-              <div className="flex justify-center items-center">
-                <p className="font-bold w-full text-center">Stock</p>
+              <div className="col-span-1 hidden lg:block md:grid p-2">
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-center">Stock</p>
+                </div>
               </div>
-            </div>
-          </button>
-          <Pagination items={filtered}></Pagination>
+            </button>
+            <Pagination items={filtered}></Pagination>
+          </div>
         </div>
+
       </div>
     </div>
   );
