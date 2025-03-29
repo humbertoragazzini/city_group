7d1564b925880341bb297c87a39567900bcaa300
humberto ragazzini <beto_vicio@hotmail.com>
2025-03-29 19:12:13 +0100

    adding cards

--- src/app/Catalogue/page.tsx
+++ src/app/Catalogue/page.tsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 import { RiCreativeCommonsZeroLine } from "react-icons/ri";
 import gsap from "gsap";
 import { useFilteredProducts } from "./useFilteredProducts";
-import products from "./products.tsx"
+import products from "./products.tsx";
 import Pagination from "./paginator";
 import Example from "./input";
 import BeamContainer from "./input";
@@ -23,15 +23,21 @@ export default function Catalogue() {
   const [selected, setSelected] = useState("All");
   const [subSelected, setSubSelected] = useState("All");
   const resultContainerRef = useRef();
-  const filtered = useFilteredProducts(products, wordToSearch, IDtoSearch, category, subCategory);
+  const filtered = useFilteredProducts(
+    products,
+    wordToSearch,
+    IDtoSearch,
+    category,
+    subCategory
+  );
   const [sortedBy, setSortedBy] = useState(null);
   const [mode, setMode] = useState("dark");
   const [filterObj, setFilterObj] = useState({
     id: -1,
     name: "",
     category: [""],
-    subCategory: [""]
-  })
+    subCategory: [""],
+  });
 
   useEffect(() => {
     if (context.state.isMenuOpen) {
@@ -44,73 +50,199 @@ export default function Catalogue() {
     }
   }, [context.state.isMenuOpen]);
 
-
   return (
     <div
-      className={`relative z-10 transition-all duration-1000 m-auto pt-[0px] bg-gradient-to-b ${mode == "dark" ? "from-[rgba(0,0,0,1)] to-[rgba(0,0,0,1)]" : "from-[rgba(255,255,255,1)] to-[rgba(150,150,150,1)]"} min-h-screen w-full text-white`}
+      className={`relative z-10 transition-all duration-1000 m-auto pt-[0px] bg-gradient-to-b ${
+        mode == "dark"
+          ? "from-[rgba(0,0,0,1)] to-[rgba(0,0,0,1)]"
+          : "from-[rgba(255,255,255,1)] to-[rgba(150,150,150,1)]"
+      } min-h-screen w-full text-white`}
     >
       <ToggleWrapper mode={mode} setMode={setMode}></ToggleWrapper>
       <div className="w-full max-w-[1450px] mx-auto mt-8">
         <div className="px-8">
           <BeamContainer mode={mode}>
             <div className="grid grid-cols-4">
-              <div className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2 ${mode == "dark" ? "text-white" : "text-black"} rounded-lg`}>
+              <div
+                className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2 ${
+                  mode == "dark" ? "text-white" : "text-black"
+                } rounded-lg`}
+              >
                 <label className="mr-2">ID:</label>
-                <input onKeyUp={(e) => { setIDtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by ID">
-                </input>
+                <input
+                  onKeyUp={(e) => {
+                    setIDtoSearch(e.currentTarget.value);
+                  }}
+                  className="bg-transparent flex justify-center items-center p-2"
+                  placeholder="Search by ID"
+                ></input>
               </div>
-              <div className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2  ${mode == "dark" ? "text-white" : "text-black"} rounded-lg`}>
+              <div
+                className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2  ${
+                  mode == "dark" ? "text-white" : "text-black"
+                } rounded-lg`}
+              >
                 <label className="mr-2 text-nowrap">By name:</label>
-                <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by word">
-                </input>
+                <input
+                  onKeyUp={(e) => {
+                    setWordtoSearch(e.currentTarget.value);
+                  }}
+                  className="bg-transparent flex justify-center items-center p-2"
+                  placeholder="Search by word"
+                ></input>
               </div>
-              <div className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2  ${mode == "dark" ? "text-white" : "text-black"} rounded-lg`}>
+              <div
+                className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2  ${
+                  mode == "dark" ? "text-white" : "text-black"
+                } rounded-lg`}
+              >
                 <label className="mr-2">By category:</label>
                 {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
-                <DropdownMenu category={category}
-                  setCategory={setCategory} selected={selected}
-                  setSelected={setSelected} types={products} products={products} type="category"></DropdownMenu>
+                <DropdownMenu
+                  category={category}
+                  setCategory={setCategory}
+                  selected={selected}
+                  setSelected={setSelected}
+                  types={products}
+                  products={products}
+                  type="category"
+                ></DropdownMenu>
               </div>
-              <div className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2  ${mode == "dark" ? "text-white" : "text-black"} rounded-lg`}>
+              <div
+                className={`col-span-4 md:col-span-2 xl:col-span-1 px-0 py-0 flex justify-start xl:justify-center items-center font-semibold mx-2  ${
+                  mode == "dark" ? "text-white" : "text-black"
+                } rounded-lg`}
+              >
                 <label className="mr-2">By subcategory:</label>
                 {/* <input onKeyUp={(e) => { setWordtoSearch(e.currentTarget.value) }} className="bg-transparent flex justify-center items-center p-2" placeholder="Search by type"> */}
-                <DropdownMenu category={subCategory}
-                  setCategory={setSubCategory} selected={subSelected} filtered={filtered}
-                  setSelected={setSubSelected} types={products} products={products}></DropdownMenu>
+                <DropdownMenu
+                  category={subCategory}
+                  setCategory={setSubCategory}
+                  selected={subSelected}
+                  filtered={filtered}
+                  setSelected={setSubSelected}
+                  types={products}
+                  products={products}
+                ></DropdownMenu>
               </div>
             </div>
           </BeamContainer>
         </div>
 
         {/* Section with us and the description */}
-        <div ref={resultContainerRef} className="w-full flex flex-col justify-start items-center overflow-hidden relative">
+        <div
+          ref={resultContainerRef}
+          className="w-full flex flex-col justify-start items-center overflow-hidden relative"
+        >
           <div className="grid w-full grid-cols-1 p-4 lg:p-8">
             <button className="relative z-10 grid grid-cols-12 full backdrop-blur-md p-2 rounded-xl cursor-pointer w-full transition-all duration-300">
-              <div className={`col-span-3 md:col-span-2 p-2 border-r-2 ${mode == "dark" ? "text-white border-[rgba(255,255,255,0.5)]" : "text-black border-[rgba(150,150,150,0.5)]"}`}>
+              <div
+                className={`col-span-3 md:col-span-2 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
                 <div className="flex justify-center items-center">
                   <p className="font-bold w-full text-center">ID</p>
                 </div>
               </div>
-              <div className={`col-span-3 md:col-span-6 p-2 border-r-2 ${mode == "dark" ? "text-white border-[rgba(255,255,255,0.5)]" : "text-black border-[rgba(150,150,150,0.5)]"}`}>
+              <div
+                className={`col-span-3 md:col-span-6 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
                 <div className="flex justify-center items-center">
                   <p className="font-bold w-full text-left">Name</p>
                 </div>
               </div>
-              <div className={`col-span-3 md:col-span-3 p-2 border-r-2 ${mode == "dark" ? "text-white border-[rgba(255,255,255,0.5)]" : "text-black border-[rgba(150,150,150,0.5)]"}`}>
+              <div
+                className={`col-span-3 md:col-span-3 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
                 <div className="flex justify-center items-center">
                   <p className="font-bold w-full text-center">Category</p>
                 </div>
               </div>
-              <div className={`col-span-3 md:col-span-1 p-2 border-r-2 ${mode == "dark" ? "text-white border-[rgba(255,255,255,0.5)]" : "text-black border-[rgba(150,150,150,0.5)]"}`}>
+              <div
+                className={`col-span-3 md:col-span-1 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
                 <div className="flex justify-center items-center">
                   <p className="font-bold w-full text-center">Stock</p>
                 </div>
               </div>
             </button>
-            <Pagination items={filtered} mode={mode}></Pagination>
+            <Pagination
+              items={filtered}
+              mode={mode}
+              type="listItem"
+            ></Pagination>
           </div>
         </div>
-
+        {/* Section with us and the description */}
+        <div
+          ref={resultContainerRef}
+          className="w-full flex flex-col justify-start items-center overflow-hidden relative"
+        >
+          <div className="grid w-full grid-cols-1 p-4 lg:p-8">
+            <button className="relative z-10 grid grid-cols-12 full backdrop-blur-md p-2 rounded-xl cursor-pointer w-full transition-all duration-300">
+              <div
+                className={`col-span-3 md:col-span-2 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-center">ID</p>
+                </div>
+              </div>
+              <div
+                className={`col-span-3 md:col-span-6 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-left">Name</p>
+                </div>
+              </div>
+              <div
+                className={`col-span-3 md:col-span-3 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-center">Category</p>
+                </div>
+              </div>
+              <div
+                className={`col-span-3 md:col-span-1 p-2 border-r-2 ${
+                  mode == "dark"
+                    ? "text-white border-[rgba(255,255,255,0.5)]"
+                    : "text-black border-[rgba(150,150,150,0.5)]"
+                }`}
+              >
+                <div className="flex justify-center items-center">
+                  <p className="font-bold w-full text-center">Stock</p>
+                </div>
+              </div>
+            </button>
+            <Pagination items={filtered} mode={mode} type={"card"}></Pagination>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -118,33 +250,41 @@ export default function Catalogue() {
 
 Catalogue.displayName = "Catalogue";
 
-function DropdownMenu({ category,
-  setCategory, types, selected, products,
-  setSelected, type, filtered }: any) {
-  console.log(category)
+function DropdownMenu({
+  category,
+  setCategory,
+  types,
+  selected,
+  products,
+  setSelected,
+  type,
+  filtered,
+}: any) {
+  console.log(category);
   const [isOpen, setIsOpen] = useState(false);
-  const [options, setOptions] = useState(type == "category" ? [...new Set(products.map(({ category }) => category))] : [...new Set(filtered.map(({ subcategory }) => subcategory))])
+  const [options, setOptions] = useState(
+    type == "category"
+      ? [...new Set(products.map(({ category }) => category))]
+      : [...new Set(filtered.map(({ subcategory }) => subcategory))]
+  );
 
-
-  const toggleDropdown = () => setIsOpen(prev => !prev);
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
 
   const handleSelect = (option) => {
     setSelected(option);
-    setCategory(option)
+    setCategory(option);
     setIsOpen(false);
   };
 
   useEffect(() => {
     if (type !== "category") {
-      setOptions([...new Set(filtered.map(({ subcategory }) => subcategory))])
+      setOptions([...new Set(filtered.map(({ subcategory }) => subcategory))]);
     }
-  }, [filtered])
+  }, [filtered]);
 
   return (
     <div className="relative min-w-[150px]">
-      <button onClick={toggleDropdown}>
-        {selected || "Select an option"}
-      </button>
+      <button onClick={toggleDropdown}>{selected || "Select an option"}</button>
 
       {isOpen && (
         <ul className="z-20 px-4 py-2 top-0 left-0 h-[300px] w-fit rounded-md absolute backdrop-blur-xl overflow-y-scroll overflow-x-hidden scrollbar scrollbar-thin scrollbar-thumb-rounded">
@@ -155,15 +295,16 @@ function DropdownMenu({ category,
           >
             {"All"}
           </li>
-          {options !== undefined && options.map((option, index) => (
-            <li
-              key={index}
-              onClick={() => handleSelect(option)}
-              style={{ cursor: "pointer" }}
-            >
-              {option}
-            </li>
-          ))}
+          {options !== undefined &&
+            options.map((option, index) => (
+              <li
+                key={index}
+                onClick={() => handleSelect(option)}
+                style={{ cursor: "pointer" }}
+              >
+                {option}
+              </li>
+            ))}
         </ul>
       )}
     </div>
--- src/app/Catalogue/paginator.tsx
+++ src/app/Catalogue/paginator.tsx
@@ -1,45 +1,61 @@
 //@ts-nocheck
-"use client"
+"use client";
 
 import { useState, useMemo } from "react";
 import { Item } from "./item";
+import { Card } from "./card";
 
-export default function Pagination({ items = [], itemsPerPage = 10, renderItem }) {
-    const [currentPage, setCurrentPage] = useState(1);
+export default function Pagination({
+  items = [],
+  itemsPerPage = 10,
+  renderItem,
+  type,
+}) {
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const totalPages = Math.ceil(items.length / itemsPerPage);
+  const totalPages = Math.ceil(items.length / itemsPerPage);
 
-    const currentItems = useMemo(() => {
-        const start = (currentPage - 1) * itemsPerPage;
-        return items.slice(start, start + itemsPerPage);
-    }, [items, currentPage, itemsPerPage]);
+  const currentItems = useMemo(() => {
+    const start = (currentPage - 1) * itemsPerPage;
+    return items.slice(start, start + itemsPerPage);
+  }, [items, currentPage, itemsPerPage]);
 
-    const handleNext = () => {
-        if (currentPage < totalPages) setCurrentPage(prev => prev + 1);
-    };
+  const handleNext = () => {
+    if (currentPage < totalPages) setCurrentPage((prev) => prev + 1);
+  };
 
-    const handlePrev = () => {
-        if (currentPage > 1) setCurrentPage(prev => prev - 1);
-    };
+  const handlePrev = () => {
+    if (currentPage > 1) setCurrentPage((prev) => prev - 1);
+  };
 
-    return (
-        <div>
-            <div className="grid gap-2">
-                {currentItems.map((product, index) => <Item key={index} index={index} product={product}></Item>
-                )}
-            </div>
-
-            <div className="flex justify-between items-center mt-4">
-                <button onClick={handlePrev} disabled={currentPage === 1}>
-                    ⬅ Prev
-                </button>
-                <span>
-                    Page {currentPage} of {totalPages}
-                </span>
-                <button onClick={handleNext} disabled={currentPage === totalPages}>
-                    Next ➡
-                </button>
-            </div>
+  return (
+    <div>
+      {type == "listItem" && (
+        <div className="grid gap-2">
+          {currentItems.map((product, index) => (
+            <Item key={index} index={index} product={product}></Item>
+          ))}
         </div>
-    );
+      )}
+      {type == "card" && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+          {currentItems.map((product, index) => (
+            <Card key={index} index={index} product={product}></Card>
+          ))}
+        </div>
+      )}
+
+      <div className="flex justify-between items-center mt-4">
+        <button onClick={handlePrev} disabled={currentPage === 1}>
+          ⬅ Prev
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button onClick={handleNext} disabled={currentPage === totalPages}>
+          Next ➡
+        </button>
+      </div>
+    </div>
+  );
 }
