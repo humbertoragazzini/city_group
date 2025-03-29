291e3f59e7c79ba5a3b0cdc825bde543a477630c
humberto ragazzini <beto_vicio@hotmail.com>
2025-03-29 19:32:57 +0100

    added card mode

--- src/app/Catalogue/paginator.tsx
+++ src/app/Catalogue/paginator.tsx
@@ -38,7 +38,7 @@ export default function Pagination({
         </div>
       )}
       {type == "card" && (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {currentItems.map((product, index) => (
             <Card key={index} index={index} product={product}></Card>
           ))}
